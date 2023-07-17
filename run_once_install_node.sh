b32ad782b6824c3b48f73820a1b120e111eed0c6
justjcurtis <justjcurtis@gmail.com>
2023-07-17 19:23:20 +0100

    basic install stuff

---
+++ run_once_install_node.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# install node
+nvm install 18.16.1
+
+# install yarn
+npm install -g yarn
+
+# install browser-sync
+npm install -g browser-sync
+
