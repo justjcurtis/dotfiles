459ba7eb0b3a848e0efd9f0df5313cfc42cdd9e5
justjcurtis <justjcurtis@gmail.com>
2023-07-19 10:48:10 +0100

    add 80 col rule highlight

--- dot_config/nvim/lua/justjcurtis/editor.lua
+++ dot_config/nvim/lua/justjcurtis/editor.lua
@@ -6,7 +6,12 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
 vim.opt.wrap = false
-vim.opt.scrolloff = 8
 vim.opt.smartindent = true
 vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
 vim.opt.undofile = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.colorcolumn = "80"
