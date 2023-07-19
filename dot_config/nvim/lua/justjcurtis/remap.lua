e6fcd56fec0fc8e509ebbb5736471e963ba29edb
justjcurtis <justjcurtis@gmail.com>
2023-07-19 16:43:34 +0100

    formatting remaps

--- dot_config/nvim/lua/justjcurtis/remap.lua
+++ dot_config/nvim/lua/justjcurtis/remap.lua
@@ -7,8 +7,8 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "<leader>Q", ":q!<CR>")
-vim.keymap.set("n", "<leader>w", ":w<CR>")
-vim.keymap.set("n", "<leader>W", ":w!<CR>")
+vim.keymap.set("n", "<leader>w", "<cmd>Format<CR>:w<CR>")
+vim.keymap.set("n", "<leader>W", "<cmd>Format<CR>:w!<CR>")
 
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
@@ -26,3 +26,5 @@ vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>f", "<cmd>Format<CR>", { silent = true })
