0ff77e5d3b85907bc011a08db90c79d643bb34fb
jacson.curtis <jacson.curtis@zopa.com>
2023-07-27 16:17:40 +0100

    enable word wrap && remap 'leader ,' to copy buffer path to clipboard

--- dot_config/nvim/lua/justjcurtis/editor.lua
+++ dot_config/nvim/lua/justjcurtis/editor.lua
@@ -5,7 +5,6 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
-vim.opt.wrap = false
 vim.opt.smartindent = true
 vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
 vim.opt.undofile = true
--- dot_config/nvim/lua/justjcurtis/remap.lua
+++ dot_config/nvim/lua/justjcurtis/remap.lua
@@ -28,3 +28,5 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>f", "<cmd>Format<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>,", ":let @+ = expand ('%:p')<CR>", { silent = true })
