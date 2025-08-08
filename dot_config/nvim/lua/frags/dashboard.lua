29f1f7944d16ca07c10f990f71a478d0b02c87f6
justjcurtis <justjcurtis@gmail.com>
2025-08-08 13:33:12 +0100

    ensure jea is called correctly

--- dot_config/nvim/lua/frags/dashboard.lua
+++ dot_config/nvim/lua/frags/dashboard.lua
@@ -63,7 +63,7 @@ return {
             { section = "startup", padding = 2 },
             {
                 section = "terminal",
-                cmd = "jea; sleep 0.1",
+                cmd = "/bin/bash -lc 'jea'; sleep 0.1",
                 height = 22,
                 indent = 25,
             }
